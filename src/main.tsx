1edd89651b5eeb327a200f1a559b301885dd2b82
Aielina <aielina.madambekova@gmail.com>
2024-11-18 22:16:27 +0600

    Initial commit

---
+++ src/main.tsx
@@ -0,0 +1,17 @@
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { StrictMode } from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { store } from './app/Store/store.ts';
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
+  </StrictMode>,
+);
